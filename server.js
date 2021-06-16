636e681072423416ff3f535c67f7099431db8ce3
dina-fouad <dina_albarghouthi@yahoo.com>
2021-06-16 18:21:38 +0300

    update

--- server.js
+++ server.js
@@ -5,22 +5,32 @@ const express = require('express');
 // const weatherData = require('./data/weather.json');
 const cors = require('cors');
 const axios = require('axios');
-
 const server = express();
-
 const PORT = process.env.PORT;
 
 server.use(cors());//my server can get any req from any clinet
+
 
 
 //localhost:3065/
 server.get('/weather', weatherHandler);
 server.get('/movie', movieHandler)
 
+
+
 server.get('/', (req, res) => {
     let str = 'hello from backend';
     res.send(str);
 });
+
+
+
+server.get('*', (req, res) => {
+    res.send('not found');
+})
+
+
+
 
 const weatherHandler = require('./modules/weather.js');
 const movieHandler = require('./modules/movies.js')
@@ -28,9 +38,6 @@ const movieHandler = require('./modules/movies.js')
 
 
 
-server.get('*', (req, res) => {
-    res.send('not found');
-})
 
 
 
